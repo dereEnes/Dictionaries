db9ae528d0002f0c074a1bcc5ef406b9caa793af
enes <dere.enes01@gmail.com>
2021-02-07 14:22:50 +0300

    Add project files.

---
+++ Dictionaries/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionaries
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+
+        }
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length+1];
+            values = new TValue[values.Length+1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+
+            }
+            keys[tempKeys.Length] = key;
+            values[tempValues.Length] = value;
+
+        }
+        public void ToList()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key: {0} , Value: {1}",keys[i],values[i]);
+            }
+        }
+    }
+}
---
+++ Dictionaries/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> sozluk = new MyDictionary<string, string>();
+            sozluk.Add("elma","apple");
+            sozluk.Add("fare","mouse");
+            sozluk.Add("su","water");
+            sozluk.ToList();
+        }
+    }
+}
